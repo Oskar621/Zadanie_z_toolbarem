2597af522f1e26654765cfccd64e30640e1170d4
User <asd>
2023-04-06 00:18:45 +0200

    Initial commit, added binding

---
+++ app/src/main/java/com/example/zadanie_z_toolbarem/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.zadanie_z_toolbarem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.zadanie_z_toolbarem.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    class MainActivity : AppCompatActivity() {
+        private lateinit var binding: ActivityMainBinding
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            val view = binding.root
+            setContentView(view)
+        }
+    }
+}
