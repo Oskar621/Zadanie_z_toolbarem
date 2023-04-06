04f2074af67fa37ace77b87ff3af4bf6cdc190a2
User <asd>
2023-04-06 17:51:01 +0200

    Toolbar is done, Clicking on toolbar icon makes NavigationView visable, and u can close it by pressing "Zamknij" button

--- app/src/main/java/com/example/zadanie_z_toolbarem/MainActivity.kt
+++ app/src/main/java/com/example/zadanie_z_toolbarem/MainActivity.kt
@@ -2,17 +2,28 @@ package com.example.zadanie_z_toolbarem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
 import com.example.zadanie_z_toolbarem.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
-    class MainActivity : AppCompatActivity() {
         private lateinit var binding: ActivityMainBinding
 
         override fun onCreate(savedInstanceState: Bundle?) {
+            supportActionBar?.hide() // ukrycie paska na górze
             super.onCreate(savedInstanceState)
             binding = ActivityMainBinding.inflate(layoutInflater)
             val view = binding.root
             setContentView(view)
-        }
+
+            //rozwinięcie paska
+            binding.toolbarIcon.setOnClickListener {
+                binding.navView.visibility = View.VISIBLE
+            }
+
+            binding.ButtonClose.setOnClickListener {
+                binding.navView.visibility = View.GONE
+            }
     }
 }
