79add675978074854044df0fc1018df758b58bc5
IETI <58682773+marcinkolacz@users.noreply.github.com>
2023-04-13 10:33:09 +0200

    changed colors in grades table added standard layout in user informations

--- app/src/main/java/com/example/zadanie_z_toolbarem/GradesTable.kt
+++ app/src/main/java/com/example/zadanie_z_toolbarem/GradesTable.kt
@@ -3,18 +3,17 @@ package com.example.zadanie_z_toolbarem
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.zadanie_z_toolbarem.databinding.ActivityMainBinding
 
 class GradesTable : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_grades_table)
-
         supportActionBar?.hide() // ukrycie defaultowego topbara
 
         // Wczytanie danych uzytkownika
-        val User_Data = intent.getStringArrayExtra("userinfo")
-        findViewById<TextView>(R.id.toolbarText).text = "${User_Data?.get(1)} ${User_Data?.get(2)}"
-        findViewById<TextView>(R.id.navUsername).text = User_Data?.get(0)
+        val userData = intent.getStringArrayExtra("userinfo")
+        findViewById<TextView>(R.id.toolbarText).text = "${userData?.get(1)} ${userData?.get(2)}"
+        findViewById<TextView>(R.id.navUsername).text = userData?.get(0)
     }
 }
